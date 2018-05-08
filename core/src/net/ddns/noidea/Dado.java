5c8d923a8ac0acbc16580027c1572dbd9808a97e
franciscoquintana <francisco.quintana.dosa@gmail.com>
2018-05-09 00:17:35 +0200

    El dado ahora usa una sola imagen para su textura

--- core/src/net/ddns/noidea/Dado.java
+++ core/src/net/ddns/noidea/Dado.java
@@ -1,26 +1,29 @@
 package net.ddns.noidea;
 
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
 import java.util.ArrayList;
 
 public class Dado {
-    Texture img;
 
     ArrayList<Texture> texturas = new ArrayList<Texture>();
+
+    TextureRegion[][] regions;
 
     private Integer numero;
 
     public Dado() {
-        for (int i = 1; i <= 6; i++) {
+        /*for (int i = 1; i <= 6; i++) {
             texturas.add(new Texture("dado"+i+".gif"));
-        }
+        }*/
+        Texture texture = new Texture("dadosjuntos.png");
+        regions = TextureRegion.split(texture,200 ,200);
         setNumber(1);
     }
 
     public void setNumber(int number) {
         numero = number;
-        img = texturas.get(number-1);
     }
 
     public Integer getNumber() {
@@ -28,9 +31,9 @@ public class Dado {
     }
 
 
-
-
-    public Texture getImg() {
-        return img;
+    public TextureRegion getImg() {
+        int a = numero / 3 >= 1 ? 1 : 0;
+        int b = numero > 3 ? numero -3 : numero;
+        return regions[a][b-1];
     }
 }
