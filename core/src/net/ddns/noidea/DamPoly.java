59c97b5e9b88e7ad7059d4b78d784eaaf87f639c
Bruno <amor95_8@gmail.com>
2018-05-15 20:56:09 +0200

    Merge branch 'dev-gamelayer' of D:\Bruno\Documents\sa with conflicts.

--- core/src/net/ddns/noidea/DamPoly.java
+++ core/src/net/ddns/noidea/DamPoly.java
@@ -4,15 +4,12 @@ import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import net.ddns.noidea.Graphics.GameLayer;
-import net.ddns.noidea.Graphics.GameScene;
 import net.ddns.noidea.internal.Tablero;
 
 public class DamPoly extends ApplicationAdapter {
 	SpriteBatch batch;
 	Dado dados[];
 	Tablero tablero;
-	GameScene gameScene;
 
 	private boolean showDados = false;
 
@@ -39,22 +36,18 @@ public class DamPoly extends ApplicationAdapter {
 		dados[0] = new Dado();
 		dados[1] = new Dado();
 		tablero = Tablero.setInstance(this);
-		gameScene = new GameScene();
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClearColor(1, 1, 1, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
 		if (isShowDados()) {
 			batch.draw(dados[0].getImg(), 0, 0);
 			batch.draw(dados[1].getImg(), 250, 0);
 		}
-		gameScene.act();
-		gameScene.draw();
 		batch.end();
-		Gdx.input.setInputProcessor(gameScene);
 	}
 	
 	@Override
