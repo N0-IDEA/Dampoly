70fcc09b8e4547304e3f4ccb38c95a9d18919d25
cesfernandez1 <cesarfer9824@gmail.com>
2018-05-21 19:42:31 +0200

    Añadido hipotecar y deshipotecar

---
+++ desktop/src/net/ddns/noidea/desktop/Commands/DeshipotecarCommand.java
@@ -0,0 +1,38 @@
+package net.ddns.noidea.desktop.Commands;
+
+import net.ddns.fquintana.ConsoleCommands.CommandsCore.ColoredConsole;
+import net.ddns.fquintana.ConsoleCommands.CommandsCore.CommandSingle;
+import net.ddns.noidea.desktop.DesktopLauncher;
+import net.ddns.noidea.internal.Casillas.Casilla;
+import net.ddns.noidea.internal.Casillas.PropiedadCasilla;
+import net.ddns.noidea.internal.Jugador;
+import net.ddns.noidea.internal.Tablero;
+
+public class DeshipotecarCommand  extends CommandSingle{
+    private static final Object HipotecarCommand = true;
+
+    public DeshipotecarCommand(){ super( "Deshipotecar", "", "Deshipotecar propiedades",0); }
+
+    @Override
+    public boolean run (ColoredConsole coloredConsole, String[] strings) {
+        Tablero tablero = DesktopLauncher.getDamPoly().getTablero();
+        Jugador jugador = tablero.getJugadorActual();
+
+        Casilla casilla = jugador.getCasillaActual();
+
+        if(casilla instanceof PropiedadCasilla) {
+            PropiedadCasilla propiedadCasilla = (PropiedadCasilla) casilla;
+            coloredConsole.sendMessage("Has deshipotecado esta casilla " + propiedadCasilla.getNombre());
+            jugador.dinero -= propiedadCasilla.hipoteca;
+        }else {
+            PropiedadCasilla propiedadCasilla = (PropiedadCasilla) casilla;
+            propiedadCasilla.propietario = jugador;
+                coloredConsole.error("No puedesdeshipotecar esta casilla porque no esta previamente hipotecada" );
+
+
+
+
+        }
+        return true;
+    }
+}
