9c5cc1eb6759a551cec854eab1182dd71731dc2a
KelvinJoelC <kelvinjcarreno@gmail.com>
2018-05-23 00:18:28 +0200

    Imagen tablero

--- core/src/net/ddns/noidea/DamPoly.java
+++ core/src/net/ddns/noidea/DamPoly.java
@@ -2,10 +2,8 @@ package net.ddns.noidea;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import net.ddns.noidea.Graphics.GameLayer;
 import net.ddns.noidea.Graphics.GameScene;
 import net.ddns.noidea.internal.Ficha;
 import net.ddns.noidea.internal.Jugador;
